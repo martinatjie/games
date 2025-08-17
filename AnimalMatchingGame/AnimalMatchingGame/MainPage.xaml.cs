68f2486b9d142bf4000c9ddbbc9dd27b0222dc6f
Martinatjie <martinabrockmann@gmail.com>
2025-08-17 09:06:53 +0200

    created animal matching card game with timer, based on head first csharp book

---
+++ AnimalMatchingGame/AnimalMatchingGame/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿namespace AnimalMatchingGame
+{
+    public partial class MainPage : ContentPage
+    {
+        Button lastClicked;
+        bool findingMatch = false;
+        int matchesFound;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayAgainButton_Clicked(object sender, EventArgs e)
+        {
+            AnimalButtons.IsVisible = true;
+            PlayAgainButton.IsVisible = false;
+
+            var animalEmoji = new List<string>
+            {
+                "🐶","🐶",
+                "🐭","🐭",
+                "🐰","🐰",
+                "🐔","🐔",
+                "🐼","🐼",
+                "🐯","🐯",
+                "🐮","🐮",
+                "🐸","🐸",
+            };
+
+            foreach (var button in AnimalButtons.Children.OfType<Button>())
+            {
+                int index = Random.Shared.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                button.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+
+            Dispatcher.StartTimer(TimeSpan.FromSeconds(.1), TimerTick);
+        }
+
+        int tenthsOfSecondsElapsed = 0;
+
+        private bool TimerTick()
+        {
+            if (!this.IsLoaded) return false;
+            tenthsOfSecondsElapsed++;
+            TimeElapsed.Text = "Time elapsed: " +
+            (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+            if (PlayAgainButton.IsVisible)
+            {
+                tenthsOfSecondsElapsed = 0;
+                return false;
+            }
+            return true;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (sender is Button buttonClicked)
+            {
+                if (!string.IsNullOrWhiteSpace(buttonClicked.Text) && (findingMatch == false))
+                {
+                    buttonClicked.BackgroundColor = Colors.Red;
+                    lastClicked = buttonClicked;
+                    findingMatch = true;
+                }
+                else
+                {
+                    if ((buttonClicked != lastClicked) && (buttonClicked.Text == lastClicked.Text) && (!String.IsNullOrWhiteSpace(buttonClicked.Text)))
+                    {
+                        matchesFound++;
+                        lastClicked.Text = " ";
+                        buttonClicked.Text = " ";
+                    }
+                    lastClicked.BackgroundColor = Colors.LightBlue;
+                    buttonClicked.BackgroundColor = Colors.LightBlue;
+                    findingMatch = false;
+                }
+            }
+            if (matchesFound == 8)
+            {
+                matchesFound = 0;
+                AnimalButtons.IsVisible = false;
+                PlayAgainButton.IsVisible = true;
+            }
+        }
+    }
+}
